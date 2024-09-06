92d03ee230313a481e551526822dc68b728338c0
ranokhassanova <khasanova.rano@gmail.com>
2024-09-06 21:48:34 +0500

    create sidebar & Mobile Nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,33 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
 import type { Metadata } from "next";
-import { Inter, IBM_Plex_Serif } from "next/font/google";
+import Image from "next/image";
 
 
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <main>
-        {children}
-    </main>
-  );
+	const loggedIn = { firstName: 'Rano', lastName: 'Eesydev' }
+	return (
+		<main className="flex h-screen w-full font-inter">
+			<Sidebar user={loggedIn} />
+			<div className="flex size-full flex-col">
+				<div className="root-layout">
+					<Image
+						src="/icons/logo.svg"
+						alt="menu icon"
+						width={30}
+						height={30}
+					/>
+					<div className="">
+						<MobileNav user={loggedIn} />
+					</div>
+				</div>
+				{children}
+			</div>
+		</main>
+	);
 }
