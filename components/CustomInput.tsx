a91caa08bece7e18544561549f6b9033624cbc6c
ranokhassanova <khasanova.rano@gmail.com>
2025-05-24 20:08:42 +0500

    implement form, separate component of Input

---
+++ components/CustomInput.tsx
@@ -0,0 +1,48 @@
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
+import { authFormSchema } from "@/lib/utils";
+
+interface CustomInput {
+    control: Control<z.infer<typeof authFormSchema>>,
+    name: FieldPath<z.infer<typeof authFormSchema>>,
+    type: string,
+    label: string,
+    placeholder: string
+}
+
+const CustomInput = ({ control, name, type, label, placeholder } : CustomInput) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <div className="form-item">
+                    <FormLabel className="form-label">{label}</FormLabel>
+                    <div className="flex w-full flex-col">
+                        <FormControl>
+                            <Input
+                                className="input-class"
+                                placeholder={placeholder}
+                                type={type}
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormMessage className="form-message mt-2" />
+                    </div>
+                </div>
+            )}
+        />
+    )
+}
+
+export default CustomInput
